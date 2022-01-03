771c8eaec99e2ea8c5531aa0d611deb40015cfaa
naveenkmavoor <naveenkmavoor1@gmail.com>
2022-01-03 22:14:27 +0530

    feat: fetch data from api and parse into models

--- lib/data/network_service.dart
+++ lib/data/network_service.dart
@@ -1,1 +1,23 @@
-class NetworkService {}
+import 'dart:convert';
+
+import 'package:task/data/models/screenTime.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkService {
+  static const url = 'https://api.mocklets.com/p68289/screentime';
+
+  Future<ScreenTime> fetchScreenTimeParameters() async {
+    try {
+      final http.Response response = await http.get(Uri.parse(url));
+      print(response.body);
+      if (response.statusCode >= 400) {
+        throw ('Failed to load data');
+      }
+      final val = json.decode(response.body);
+      final screenTimeList = val.map((e) => ScreenTime.fromJson(e)).toList();
+      return screenTimeList[0];
+    } catch (err) {
+      throw ('Failed to load data');
+    }
+  }
+}
--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,1 +1,12 @@
-class Repository {}
+import 'package:task/data/models/screenTime.dart';
+import 'package:task/data/network_service.dart';
+
+class Repository {
+  final NetworkService networkService;
+  Repository({required this.networkService});
+
+  Future<ScreenTime> fetchParameters() async {
+    final screenTime = await networkService.fetchScreenTimeParameters();
+    return screenTime;
+  }
+}
