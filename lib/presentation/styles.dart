52c2deeebc7a8120af82dfaccdf061c99039de39
naveenkmavoor <naveenkmavoor1@gmail.com>
2022-01-03 22:20:26 +0530

    feat: add text style constants

---
+++ lib/presentation/styles.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const textStyle1 = TextStyle(
+    fontFamily: 'SegoeUI',
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+    color: Color(0xFF0C8722));
+
+const textStyle2 = TextStyle(
+  fontFamily: 'SegoeUI',
+  fontWeight: FontWeight.w600,
+  fontSize: 16,
+);
+
+const textStyle3 =
+    TextStyle(fontFamily: 'SegoeUI', fontWeight: FontWeight.w700, fontSize: 24);
+
+const textStyle4 = TextStyle(
+    fontFamily: 'SegoeUI',
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    color: Color(0xFF2F80ED));
+
+const textStyle5 = TextStyle(
+    fontFamily: 'SegoeUI',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+    color: Color(0xFF2F80ED));
+
+const textStyle6 = TextStyle(
+  fontFamily: 'SegoeUI',
+  fontWeight: FontWeight.w600,
+  fontSize: 18,
+);
+const textStyle7 = TextStyle(
+    fontFamily: 'SegoeUI',
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+    color: Color(0xFF2F80ED));
